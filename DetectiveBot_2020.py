bd4797c7b6f1b5a1524518e874e436039cd16577
Panaach <44406211+Panaach@users.noreply.github.com>
2020-05-06 01:40:34 +0200

    tueurs et description des tueurs

--- DetectiveBot_2020.py
+++ DetectiveBot_2020.py
@@ -30,87 +30,78 @@ def readXML(filename) :
 
 """ Fonction qui nettoie le texte des balises <ref></ref>"""
 def clearText(input):
-    input = input.replace("| name =", "|name=").replace("| country =", "|country=").replace("| victims =", "|victims=").replace("| beginyear =", "|beginyear=")
+    input = input.replace("[", "").replace("]", "").replace("{", "").replace("}", "").replace("*", "* ").replace(":", ": ")
     return re.sub(reg_ref, '', input)
-
-"""Fonction pour trouver tous les tueurs"""
-def getKiller(input):
-    find = False
-    killer = ""
-    killerList = []
-    
-    for i in input:
-        if (i[0] == "|"):
-            find = False
-            if (len(killer) != 0):
-                killerList.append(killer)
-                killer = ""
-        elif (i[0] == "|name="):
-            find = True
-        
-        if find == True and (i[1] == "NNP" or i[1] == "NNS"):
-            killer = killer + " " + i[0]   
-            print(killer)
-        
-    return killerList
-
-"""Fonction pour trouver tous les tueurs"""
-def getNbVictims(input):
-    find = False
-    nbVictims = ""
-    victimsList = []
-    victimsList.append("non répertorié")
-    
-    for i in input:
-        if (i[0] == "|"):
-            find = False
-        elif (i[0] == "|victims="):
-            find = True
-            if (len(nbVictims) != 0):
-                victimsList.append(nbVictims)
-                nbVictims = ""
-        
-        if find == True and (i[1] == "NNP" or i[1] == "NNS"):
-            nbVictims = nbVictims + i[0]            
-        
-    return victimsList
-     
-"""Fonction pour trouver tous les tueurs"""
-def getDate(input):
-    find = False
-    date = ""
-    dateList = []
-    dateList.append("non répertorié")
-    
-    for i in input:
-        if (i[0] == "|"):
-            find = False
-        elif (i[0] == "|beginyear="):
-            find = True
-            if (len(date) != 0):
-                dateList.append(date)
-                date = ""
-        
-        if find == True and (i[1] == "NNP" or i[1] == "NNS"):
-            date = date + i[0]            
-        
-    return dateList
 
 """ Fonction pour POS tagger du texte """
 def preprocess(input):
     text = nltk.word_tokenize(input)
     return nltk.pos_tag(text)
 
+"""Fonction pour trouver tous les tueurs"""
+def getKiller(input):
+    find = False
+    letterM = False
+    getInformation = False
+    testKiller = []
+    killerListWithM = []
+    allInformation = []
+    information = []
+    
+    for i in input:
+        if (i[0] == ":"):
+            find = False
+            if (len(testKiller) != 0):
+                letterM = False
+                for killer in testKiller:
+                    if (killer[0] == "M" and letterM == False):
+                        killerListWithM.append(testKiller)
+                        letterM = True
+                        getInformation = True
+                testKiller = []
+        elif (i[0] == "*"):
+            find = True 
+        elif find == True and (i[1] == "NNP" or i[1] == "NNS"):            
+            testKiller.append(i[0]) 
+            
+        """Pour trouver les informations"""
+        if(i[1] != '.' and getInformation == True):
+            information.append(i)
+        elif (getInformation == True) :
+            allInformation.append(information)
+            getInformation = False
+            information = []  
+    show(killerListWithM, allInformation)
 
+"""Fonction pour afficher les résultats"""
+def show(killerListWithM, allInformation):
+    name = ""
+    k = -1
+    details(allInformation[k])
+    for listK in killerListWithM:
+        for i in listK:
+            name += i + " "
+        print(name + ":")
+        details(allInformation[k])
+        name = ""
+        k = k + 1
+
+"""Fonction pour afficher les détails de la liste informations"""
+def details(information):
+    affichage = ""
+    for i in information: 
+        print
+        affichage += i[0] + " "
+    affichage += "\n\n"
+    print(affichage)
+    return affichage
+        
 """ Appel des fonctions """
 file = filechooser()
 text = readXML(file)
 cleanText = clearText(text)
 tagged_cleanText = preprocess(cleanText)
-print(cleanText)
 killer = getKiller(tagged_cleanText)
-date = getDate(tagged_cleanText)
-victims = getNbVictims(tagged_cleanText)
-print(killer, date, victims)
+
     
 
